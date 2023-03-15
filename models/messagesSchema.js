bd1ec3f9770510ae4d77c7050445019e5146f963
cheraph#6290 <125369139+cheraphdev@users.noreply.github.com>
2023-03-15 14:56:46 +0100

    Update messagesSchema.js

--- models/messagesSchema.js
+++ models/messagesSchema.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-    GuildId: String,
-    UserId: String,
-    UserMessages: Number
+    GuildId: { type: String },
+    UserId: { type: String},
+    UserMessages: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("messagesSchema", messageSchema);
